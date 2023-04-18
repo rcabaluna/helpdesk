da184c0afe917ba3986aa29200d5ef2dce27357d
Ruel Cabaluna Jr <rcabalunajr@gmail.com>
2023-04-18 16:37:12 +0800

    Updated 2023-04-18

--- helpdesk/app/Views/request/get-units.php
+++
@@ -1,9 +0,0 @@
-<label for="selunit">Unit</label>
-<div>
-    <select name="unit" id="selunit" class="form-control" required>
-        <option></option>
-        <?php foreach ($unit as $unitRow) { ?>
-            <option value="<?=$unitRow['unitid']?>"><?=$unitRow['name']?></option>
-        <?php } ?>
-    </select>
-</div>
